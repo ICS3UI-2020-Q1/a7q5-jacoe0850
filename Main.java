101e2382ce6c123c830d4adaf76232ecb5018a01
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-14 12:20:55 +0000

    Finished first digit method

--- Main.java
+++ Main.java
@@ -4,21 +4,21 @@
  */
 public class Main {
   // creates a procedure method with method name firstDigit and paramater is whatever integer they enter
-public static void firstDigit(int num){
+public static int firstDigit(int num){
   // if statement that mutliplies any negative number by -1
 if(num < 0){
   num = num*-1;
 }
-while(num > 10){
+while(num >= 10){
   num = num / 10;
 }
 //prints and calculates the final digit
-    System.out.println(num % 10);
+return num % 10;
 }
   
   public static void main(String[] args) {
-    firstDigit(-87893);
-    
+    int answer = firstDigit(-87893);
+    System.out.println(answer);
     
   }
 }
